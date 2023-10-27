448b80a6dd05daf6309b971f40aa1c4dbaf82c39
Baterik <erik_bonou2@hotmail.com>
2023-10-26 21:42:25 -0600

    Ejemplos y repasos de Modules Export e Import en JS

---
+++ js-pre-react/modulos-js/common-js/export.js
@@ -0,0 +1,14 @@
+// javascrit engine = motor de javascript
+// ARCHIVO PARA GUARDAR ALERTAS DE MI APLICACION
+
+const error = "ERROR"
+const warning = "WARNING"
+const info = "INFO"
+const flag = "FLAG"
+
+// SI QUEREMOS EXPORTAR A OTROS ARCHIVOS JS SE COLOCA
+
+module.exports.error = error
+module.exports.warning = warning
+module.exports.info = info
+module.exports.flag = flag
---
+++ js-pre-react/modulos-js/common-js/import.js
@@ -0,0 +1,4 @@
+// Lo que importamos es un objeto, por lo que se tiene que desestructurar
+const {error, info} = require("./export.js")
+
+console.log("Esta es una alerta importada de tipo: ", info);
